249cfde39a4f5e4856f1adcbe4a3092b6e130a8f
Aurko264 <aurko264@gmail.com>
2024-08-24 00:32:54 +0530

    update gcd

--- java/FunctionGCd.java
+++ java/FunctionGCd.java
@@ -4,17 +4,20 @@ import java.util.*;
 
 public class FunctionGCd {
     public static void printGCD(int a , int b){
-
-        while(a != b){  
-            while(a % b == 0|| b % a == 0){         
-                if(a>b) {
-                    a = a - b;
-                } else {
-                    b = b - a;
+        while(a != b){ 
+            if(a>b){
+                 a = a - b;
+            }else{
+                 b = b - a;
+            }if (a % b == 0 && b % a  == 0) {
+                if(a>b){
+                    System.out.println(a);
+                }else{
+                    System.out.println(b);
                 }
-           }
-       }
-   } 
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
