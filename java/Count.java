7b5152e9c672fde3b88148bf80f40152e6139b04
Aurko264 <aurko264@gmail.com>
2024-08-23 20:15:14 +0530

    update question

---
+++ java/Count.java
@@ -0,0 +1,14 @@
+//	Write a program to enter the numbers till the user wants and at the end it should display the count of positive, negative and zeros entered. 
+
+import java.util.*;
+
+public class Count {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the no = ");
+        int n = s.nextInt();
+
+
+        s.close();
+    }
+}
---
+++ java/Infinite.java
@@ -0,0 +1,12 @@
+// Write an infinite loop using do while condition.
+
+
+public class Infinite {
+    public static void main(String[] args) {
+          int n = 1;
+          do{
+            System.out.println(n);
+            n++; 
+          }while(n>1);
+    }
+}
