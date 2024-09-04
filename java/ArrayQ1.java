6f7bdc598c521110c59cf1184abed542fe697194
Aurko264 <aurko264@gmail.com>
2024-09-04 23:47:13 +0530

    update array

--- java/ArrayQ1.java
+++ java/ArrayQ1.java
@@ -3,5 +3,26 @@
 import java.util.*;
 
 public class ArrayQ1 {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the size : ");
+        int size = s.nextInt();
+        int numbers[] = new int[size];
+        
+        //Input
+        for (int i = 0; i < size; i++) {
+            numbers[i] = s.nextInt();
+        }
+
+        System.out.print("Enter the no you want to look for : ");
+        int x = s.nextInt();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] == x){
+                System.out.println("x found at index : " + i);
+            }
+        }
+
+        s.close();
+    }
 }
