f2512766278ef5e953624f838a29fffda8789e97
Aurko264 <aurko264@gmail.com>
2024-09-05 23:59:34 +0530

    update array

--- java/ArrayQ3.java
+++ java/ArrayQ3.java
@@ -5,5 +5,35 @@ import java.util.*;
 public class ArrayQ3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter the size : ");
+        int size = s.nextInt();
+        int number[] = new int[size];
+
+        System.out.print("Enter the numbers : ");
+
+        //Input
+        for (int i = 0; i < size; i++) {
+            number[i] = s.nextInt();
+        }
+
+        //Storing minimun and maximum value
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        //Output
+        for (int i = 0; i < number.length; i++) {
+            if(number[i]<min){
+                min = number[i];
+            }
+
+            if (number[i]>max) {
+                max = number[i];
+            }
+        }
+
+        System.out.println("The largest no is : " + max);
+        System.out.println("The smallest no is : " + min);
+
+        s.close();
     }
 }
---
+++ java/ArrayQ4.java
@@ -0,0 +1,9 @@
+//Take an array of numbers as input and check if it is an array sorted in ascending order.
+
+import java.util.*;;
+
+public class ArrayQ4 {
+    public static void main(String[] args) {
+        
+    }
+}
