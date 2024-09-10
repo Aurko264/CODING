fef8aa9f5f4a35d28043c0812354907d6df3390c
Aurko264 <aurko264@gmail.com>
2024-09-11 00:24:20 +0530

    update

--- java/Q3array2D.java
+++ java/Q3array2D.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Q3array2D {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter the rows");
+        System.out.print("Enter the rows : ");
         int n = s.nextInt();
         System.out.print("Enter the columns : ");
         int m = s.nextInt();
