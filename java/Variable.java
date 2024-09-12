5f13a2464d4417ef51ba60e4d5587652c1d192db
Aurko264 <aurko264@gmail.com>
2024-09-12 23:55:33 +0530

    UPDATE JAVA

--- java/Variable.java
+++
@@ -1,9 +0,0 @@
-public class Variable {
-    public static void main(String[] args) {
-        //Variables   
-        int a = 10;
-        int b = 25;
-        int sum = a+b;
-        System.out.println(sum);              
-    }
-}
