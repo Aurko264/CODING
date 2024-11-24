ccb6e9c39189c3bf950edf733b49fbd90d90c2c1
Aurko264 <aurko264@gmail.com>
2024-11-24 12:57:49 +0530

    update string

---
+++ java/STRING/Parseint.java
@@ -0,0 +1,7 @@
+public class Parseint{
+    public static void main(String[] args) {
+        String str = "123";
+        int number = Integer.parseInt(str);
+        System.out.println(number);
+    }
+}
---
+++ java/STRING/Parsestring.java
@@ -0,0 +1,7 @@
+public class Parsestring {
+    public static void main(String[] args) {
+        int number = 123;
+        String str = Integer.toString(number);
+        System.out.println(str.length());
+    }
+}
